8d14026aebc780eb31e4b1f38e2596edf0ba0cf6
Vinicius Gregorio <vinicius.squadp@genial.com.vc>
2022-11-15 17:06:39 -0300

    feat: starting server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+const PORT = "80"
+
+type Config struct {
+}
+
+func main() {
+	app := Config{}
+
+	log.Println("Starting server on port", PORT)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", PORT),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
